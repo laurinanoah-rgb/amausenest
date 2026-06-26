4842346d16d3050cd14ee4cf0bbcd3e6e5719602
laurinanoah-rgb <laurinanoah@gmail.com>
2026-06-26 23:32:35 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+// Mäusenest Manager – Service Worker
+const CACHE_NAME = 'mausenest-v1';
+const ASSETS = [
+  './manager-app.html',
+  './manifest.json',
+  'https://fonts.googleapis.com/css2?family=Chelsea+Market&family=Nunito:wght@400;600;700;800&display=swap'
+];
+
+// Install: cache core assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// Activate: clean old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch: network first, then cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
