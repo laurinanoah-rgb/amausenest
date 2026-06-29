19f4f6c20c6a04062525ed1e4c1526b4a7417fb2
laurinanoah-rgb <laurinanoah@gmail.com>
2026-06-29 16:05:34 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,38 +1,38 @@
 // Mäusenest Manager – Service Worker
-const CACHE_NAME = 'mausenest-v1';
+const CACHE_NAME = 'mausenest-v2';
 const ASSETS = [
-  './manager-app.html',
-  './manifest.json',
-  'https://fonts.googleapis.com/css2?family=Chelsea+Market&family=Nunito:wght@400;600;700;800&display=swap'
+'./manager-app.html',
+'./manifest.json',
+'https://fonts.googleapis.com/css2?family=Chelsea+Market&family=Nunito:wght@400;600;700;800&display=swap'
 ];
 
 // Install: cache core assets
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
+event.waitUntil(
+caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+);
+self.skipWaiting();
 });
 
 // Activate: clean old caches
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
+event.waitUntil(
+caches.keys().then(keys =>
+Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+)
+);
+self.clients.claim();
 });
 
 // Fetch: network first, then cache
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request)
-      .then(res => {
-        const clone = res.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
-        return res;
-      })
-      .catch(() => caches.match(event.request))
-  );
+event.respondWith(
+fetch(event.request)
+.then(res => {
+const clone = res.clone();
+caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+return res;
+})
+.catch(() => caches.match(event.request))
+);
 });
